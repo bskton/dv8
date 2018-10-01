a6989a87875cffe31ced31c190c1b8ce2310ed14
Ilya Levin <bskton@gmail.com>
2018-10-01 21:53:13 +1000

    Adding .env to .gitigonre on first container startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,11 @@ git config --global user.name "$GIT_USER_NAME"
 git config --global user.email $GIT_USER_EMAIL
 
 if [ ! -f "angular.json" ]; then
-   ng new "$ANGULAR_APP_NAME" --directory ./ --force
+  ng new "$ANGULAR_APP_NAME" --directory ./ --force
+  git rm --cached .env
+  echo ".env" >> .gitignore
+  git add .
+  git commit -m "Ignore .env"
 fi
 
 exec "$@"
