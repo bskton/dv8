ac50a926c3152908e3cdd28123d8d7f8d707e65b
Ilya Levin <bskton@gmail.com>
2018-10-01 21:34:17 +1000

    Force to rewrite .gitignore and README.md on ng new

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ git config --global user.name "$GIT_USER_NAME"
 git config --global user.email $GIT_USER_EMAIL
 
 if [ ! -f "angular.json" ]; then
-   ng new "$ANGULAR_APP_NAME" --directory ./
+   ng new "$ANGULAR_APP_NAME" --directory ./ --force
 fi
 
 exec "$@"
