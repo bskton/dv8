4d50e82448fb9b75f43685e101a048302ae5009d
Ilya Levin <bskton@gmail.com>
2018-10-01 22:14:12 +1000

    Fix for adding .env to .gitignore

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,7 @@ git config --global user.name "$GIT_USER_NAME"
 git config --global user.email $GIT_USER_EMAIL
 
 if [ ! -f "angular.json" ]; then
-  ng new "$ANGULAR_APP_NAME" --directory ./ --force
-  git rm --cached .env
+  ng new "$ANGULAR_APP_NAME" --directory ./ --force --skip-git
   echo ".env" >> .gitignore
   git add .
   git commit -m "Ignore .env"
