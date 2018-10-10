94a72bd6a34581c8514769ecb8edc82907bae8e2
Ilya Levin <bskton@gmail.com>
2018-10-10 22:03:33 +1000

    Component creation testing
    Tests for AppComponent and SignupComponent.

--- src/app/signup/signup.component.spec.ts
+++ src/app/signup/signup.component.spec.ts
@@ -1,5 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+import { AuthService } from '../auth/auth.service';
+import { MaterialModule } from '../material.module';
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
@@ -7,8 +11,19 @@ describe('SignupComponent', () => {
   let fixture: ComponentFixture<SignupComponent>;
 
   beforeEach(async(() => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['logout']);
     TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        MaterialModule
+      ],
+      declarations: [
+        SignupComponent
+      ],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+      ]
     })
     .compileComponents();
   }));
